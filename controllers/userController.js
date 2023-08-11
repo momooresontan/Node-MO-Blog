a94aa4aeae9aec26bd0ff38b78b49579986fb496
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-11 08:56:11 +0100

    user controller modified

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,27 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
+
+const salt = bcrypt.genSaltSync(10);
 
 exports.register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
+    const hashedPasword = bcrypt.hashSync(password, salt);
     const user = await User.create({
       username,
       email,
-      password,
+      password: hashedPasword,
     });
     res.json(user);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  const comparePassword = bcrypt.compareSync(password, user.password);
+  res.json(comparePassword);
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { register } = require("../controllers/userController");
+const { login, register } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/register", register);
+
+router.post("/login", login);
 
 module.exports = router;
