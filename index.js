3b02299d4a2bf442f3f2085e1344a8639ca90902
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-11 01:03:15 +0100

    inde modified

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/dbConnect");
 
+dotenv.config();
+
+connectDB();
 const app = express();
 
+//Middlewares
 app.use(cors());
 app.use(express.json());
 
@@ -10,5 +16,7 @@ app.post("/register", (req, res) => {
   const { username, email, password } = req.body;
   res.json({ requestData: { username, email, password } });
 });
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(4000);
