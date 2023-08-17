23e827303d5afc439e82bcb09fd667ecd679fbe1
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-17 12:08:49 +0100

    index modified

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const app = express();
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 //Mounting router
 app.use("/", userRouter);
